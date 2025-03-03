ae9559b91f10581f5250daa739546abfe08c7ffd
Depray162 <fabricio1quesada@gmail.com>
2025-03-03 12:09:31 -0600

    controllers

---
+++ config/controller/clienteController.js
@@ -0,0 +1,36 @@
+const Cliente = require("./models/Cliente");
+const asyncHandler = require("express-async-handler");
+
+
+
+    exports.VerTodosClientes = asyncHandler(async(req,resizeBy,next)=>{
+
+        const clientes = await Cliente.findAll();
+        if (!clientes) {
+            return res.status().send("Actualmente no se cuenta con clientes");
+        }
+        else
+        {
+            return res.send("Los clientes actuales son:").json(clientes)
+        }
+    });
+
+
+    exports.AgregarCliente = asyncHandler(async(req,resizeBy,next)=>{
+
+        const { id, name,age } = req.body;
+
+        // Validación para evitar campos vacíos
+        if (!id || !name || !age) {
+          return res.status(400).json({ error: "Todos lo campos deben de ser rellenados" });
+        }
+      
+        // Inserta si los campos están completos
+        const nuevoCliente = await Cliente.create({ id, name, age });
+        res.status(201).json(nuevoCliente);
+ 
+    });
+
+
+
+
