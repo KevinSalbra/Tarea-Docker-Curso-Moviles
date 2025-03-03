ae503323fd7f4c7251a94895b4f6f48478120b5f
Depray162 <fabricio1quesada@gmail.com>
2025-03-03 14:04:34 -0600

    actividad controller

---
+++ config/controller/actividadController.js
@@ -0,0 +1,34 @@
+const Actividad = require("./models/Actividad");
+const Cliente = require("./models/Cliente")
+const asyncHandler = require("express-async-handler");
+
+exports.VerActividad = asyncHandler(async (req,resresizeBy,next)=> {
+const actividad = await Actividad.findByPk(req.params.ActividadID);
+
+if(!actividad){
+    return res.send("No se encontro la activadad");
+}
+res.json(actividad);
+
+});
+
+exports.CrearActividad = asyncHandler(async(req,res,next)=> {
+
+    const { ActividadID, CodigoActividad,Nombre,ClienteID } = req.body;
+
+ 
+    if (!ActividadID || !CodigoActividad || !Nombre || !ClienteID) {
+      return res.status(400).json({ error: "Todos lo campos deben de ser rellenados" });
+    }
+  
+
+    const VerificarCliente = asyncHandler.findByPk(req.params.ClienteID);
+
+    if(!VerificarCliente){
+        return res.send("No se encontro el cliente ingresado");
+    }else{
+         const nuevaActividad = await Actividad.create({ id, CodigoActividad, Nombre,ClienteID });
+    res.status(201).json(nuevaActividad); 
+    }
+
+});
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const sequelize = require("./config/database");
-const Cliente = require("./Models/Cliente");
-const Actividad = require("./Models/Actividad");
 
-sequelize.sync();
-
-app.use(express.json());
-
-const port = process.env.port || 80;
-app.listen(port, () => console.log(`Puerto Activo`));
+const port = 3000;  
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
