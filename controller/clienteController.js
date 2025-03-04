6b97b89000ef75a0598094840c3aefc3971a4f4a
Depray162 <fabricio1quesada@gmail.com>
2025-03-04 13:15:28 -0600

    express-validator

--- controller/clienteController.js
+++ controller/clienteController.js
@@ -1,5 +1,6 @@
 const Cliente = require("../Models/Cliente");
 const asyncHandler = require("express-async-handler");
+const { validationResult } = require("express-validator");
 
 exports.VerTodosClientes = asyncHandler(async (req, res, next) => {
   const clientes = await Cliente.findAll();
@@ -11,22 +12,18 @@ exports.VerTodosClientes = asyncHandler(async (req, res, next) => {
 });
 
 exports.AgregarCliente = asyncHandler(async (req, res, next) => {
-  const { id, nombre, correoElectronico, telefono } = req.body;
-
-  if (!id || !nombre || !correoElectronico || !telefono) {
-    console.log(nombre, correoElectronico, telefono);
-    return res
-      .status(400)
-      .json({ error: "Todos lo campos deben de ser rellenados" });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
 
+  const { id, nombre, correoElectronico, telefono } = req.body;
+
   const verificarID = await Cliente.findByPk(id);
-  {
-    if (verificarID) {
-      return res
-        .status(404)
-        .json({ error: "El cliente ya existe en la base de datos" });
-    }
+  if (verificarID) {
+    return res
+      .status(404)
+      .json({ error: "El cliente ya existe en la base de datos" });
   }
 
   const nuevoCliente = await Cliente.create({
