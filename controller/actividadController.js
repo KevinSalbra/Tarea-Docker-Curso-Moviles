af915b46d224b49fd9030fd8ba1e54cc6cc4c39d
KevinSalbra <kevinsalazar74.ksb@gmail.com>
2025-03-03 20:23:09 -0600

    primera version sin errores

--- controller/actividadController.js
+++ controller/actividadController.js
@@ -1,34 +1,36 @@
 const Actividad = require("../Models/Actividad");
-const Cliente = require("../Models/Cliente")
+const Cliente = require("../Models/Cliente");
 const asyncHandler = require("express-async-handler");
 
-exports.VerActividad = asyncHandler(async (req,res,next)=> {
-const actividad = await Actividad.findByPk(req.params.ActividadID);
+exports.VerActividad = asyncHandler(async (req, res, next) => {
+  const actividad = await Actividad.findByPk(req.params.id);
 
-if(!actividad){
-    return res.send("No se encontro la activadad");
-}
-res.json(actividad);
-
+  if (!actividad) {
+    return res.send("No se encontro la actividad");
+  }
+  res.json(actividad);
 });
 
-exports.CrearActividad = asyncHandler(async(req,res,next)=> {
+exports.CrearActividad = asyncHandler(async (req, res, next) => {
+  const { nombre, clienteID, fecha } = req.body;
 
-    const { ActividadID,Nombre,ClienteID } = req.body;
+  if (!nombre || !clienteID || !fecha) {
+    return res
+      .status(400)
+      .json({ error: "Todos lo campos deben de ser rellenados" });
+  }
 
- 
-    if (!ActividadID  || !Nombre || !ClienteID) {
-      return res.status(400).json({ error: "Todos lo campos deben de ser rellenados" });
-    }
-  
+  const verificarCliente = await Cliente.findByPk(clienteID);
+  if (!verificarCliente) {
+    return res
+      .status(404)
+      .json({ error: "El cliente no existe en la base de datos" });
+  }
 
-    const VerificarCliente = asyncHandler.findByPk(req.params.ClienteID);
-
-    if(!VerificarCliente){
-        return res.send("No se encontro el cliente ingresado");
-    }else{
-         const nuevaActividad = await Actividad.create({ ActividadID, Nombre,ClienteID });
-    res.status(201).json(nuevaActividad); 
-    }
-
+  const nuevaActividad = await Actividad.create({
+    nombre,
+    clienteID,
+    fecha,
+  });
+  res.status(201).json(nuevaActividad);
 });
