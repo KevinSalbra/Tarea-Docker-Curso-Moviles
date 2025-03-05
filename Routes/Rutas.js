3cf9a0fec18cd4c23c705ebfe9268797695f64a7
KevinSalbra <kevinsalazar74.ksb@gmail.com>
2025-03-04 21:05:05 -0600

    correccion de errores

--- Routes/Rutas.js
+++ Routes/Rutas.js
@@ -9,7 +9,6 @@ router.get("/", (req, res) => {
   res.send("Pagina de inicio");
 });
 
-
 //Rutas
 router.get("/clientes", asyncHandler(ClienteController.VerTodosClientes));
 
@@ -18,22 +17,24 @@ router.post(
   [
     body("id").notEmpty().withMessage("ID es requerido"),
     body("nombre").notEmpty().withMessage("Nombre es requerido"),
-    body("correoElectronico").isEmail().withMessage("Correo electronico no es válido"),
-    body("telefono").isNumeric().withMessage("Telefono debe ser numerico")
+    body("correoElectronico")
+      .isEmail()
+      .withMessage("Correo electronico no es valido"),
+    body("telefono").isNumeric().withMessage("Telefono debe ser numerico"),
   ],
   asyncHandler(ClienteController.AgregarCliente)
 );
 
-
 router.get("/actividades/:id", asyncHandler(ActividadController.VerActividad));
-
 
 router.post(
   "/actividades",
   [
     body("nombre").notEmpty().withMessage("Nombre es requerido"),
     body("clienteID").notEmpty().withMessage("ClienteID es requerido"),
-    body("fecha").matches(/^\d{4}\/\d{2}\/\d{2}$/).withMessage("Fecha debe ser en el formato YYYY/MM/DD")
+    body("fecha")
+      .matches(/^\d{4}\/\d{2}\/\d{2}$/)
+      .withMessage("Fecha debe ser en el formato YYYY/MM/DD"),
   ],
   asyncHandler(ActividadController.CrearActividad)
 );
